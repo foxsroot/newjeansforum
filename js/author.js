45089f072c2f4ae05914b745dd3169ab055814ef
hans loe <ishigoemon878@gmail.com>
2023-11-19 21:42:57 +0700

    update responsive

--- js/author.js
+++ js/author.js
@@ -1,7 +1,23 @@
+var sosmed = [['https://github.com/foxsroot',
+				'https://www.linkedin.com/in/jerryco-axel-dinata-35115321b',
+				'https://www.instagram.com/jer.dinata/'],
+				
+			  ['https://github.com/anrexus7',
+				'https://www.linkedin.com/in/hans-38a689297',
+				'https://www.instagram.com/640509_040148/'],
+				
+			  ['https://github.com/adrianrenardkristanto',
+				'',
+				'https://instagram.com/eidorian_681'],
+			  
+			  ['https://github.com',
+				'https://www.linkedin.com',
+				'https://instagram.com']]
+
 $(document).ready(function () {
-    $('.images').hover(function () {
+    $('img').hover(function () {
 			var ini = $(this);
-            ini.siblings('.content').animate({ top: '30px' },500);
+            $(this).siblings('div.content').animate({ top: '30px' },500);
 			setTimeout(function () {
 				ini.css({transform:'scale(1.2)', transition:'transform 450ms ease-in'});
 			}, 50); 
@@ -9,9 +25,36 @@ $(document).ready(function () {
         },
         function () {
 			var ini = $(this);
-            ini.siblings('.content').animate({ top: '0px' }, 500);
+            $(this).siblings('div.content').animate({ top: '0px' }, 500);
 				setTimeout(function () {
 					ini.css({transform:'scale(1)', transition:'transform 450ms ease-out'});
 				}, 50); 
-        });
+        }
+	);
+	
+	$('.socials').on('click', function() {
+		var list = $(this).closest('li.author').index();
+		var media,person;
+		if ($(this).parent().hasClass('social j')){
+			person = list
+		}else if ($(this).parent().hasClass('social h')){
+			person = list
+		}else if ($(this).parent().hasClass('social a')){
+			person = list
+		}else {
+			person = list
+		}
+		switch ($(this).index()){
+				case 0 :
+				media = $(this).index();
+				break;
+				case 1 :
+				media = $(this).index();
+				break;
+				case 2 :
+				media = $(this).index();
+				break;
+		}
+		window.open(sosmed[person][media]);
+	});
 });
