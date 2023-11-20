9c9f36829c11f5e735a90343e8d8821704d18071
unknown <foxsroot@gmail.com>
2023-11-21 01:16:55 +0700

    update edan

---
+++ js/forums.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    // Fungsi untuk menambahkan posting baru
+    $("form").submit(function (event) {
+      event.preventDefault();
+
+      var judul = $("#judul").val();
+      var isi = $("#isi").val();
+
+      if (judul !== "" && isi !== "") {
+        var postingBaru = $(
+          "<li><h3>" + judul + "</h3><p>" + isi + "</p></li>"
+        );
+        $("ul").append(postingBaru);
+
+        $("form")[0].reset();
+      } else {
+        alert("Mohon lengkapi semua kolom input.");
+      }
+    });
+  });
