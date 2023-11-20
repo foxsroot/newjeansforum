c378e4560fd938e192a4cf7e82d59ece6534a1f4
unknown <foxsroot@gmail.com>
2023-11-21 00:08:56 +0700

    update besar besaran patch hans internet hilang

--- js/background_video.js
+++ js/background_video.js
@@ -1,32 +1,50 @@
-var video = document.getElementById("videoBackground");
-var muteButton = document.getElementById("muteButton");
-var moving = false;
-var offsetX = 0;
-var offsetY = 0;
+// var video = document.getElementById("videoBackground");
+// var muteButton = document.getElementById("muteButton");
+// var moving = false;
+// var offsetX = 0;
+// var offsetY = 0;
 
-muteButton.addEventListener("mousedown", function (e) {
-  moving = true;
-  offsetX = e.clientX - muteButton.offsetLeft;
-  offsetY = e.clientY - muteButton.offsetTop;
-  // muteButton.style.pointerEvents = 'none';
+let arrIcon = ["./assets/icon/sound_mute.png", "./assets/icon/sound_loud.png"];
+let volume = ["0", "1"];
+
+let indexButton = 0;
+
+$(document).ready(function() {
+    $("#mtbtn").attr("src", arrIcon[indexButton]);
+
+    $("#mtbtn").click(function() {
+        $("#bg-audio")[0].play();
+        indexButton = 1 - indexButton; 
+
+        $("#mtbtn").attr("src", arrIcon[indexButton]);
+        $("#bg-audio").prop("volume", volume[indexButton]);
+    });
 });
 
-document.addEventListener("mousemove", function (e) {
-  if (moving) {
-    muteButton.style.left = e.clientX - offsetX + "px";
-    muteButton.style.top = e.clientY - offsetY + "px";
-  }
-});
 
-document.addEventListener("mouseup", function () {
-  moving = false;
-  // muteButton.style.pointerEvents = 'none';
-});
+// muteButton.addEventListener("mousedown", function (e) {
+//   moving = true;
+//   offsetX = e.clientX - muteButton.offsetLeft;
+//   offsetY = e.clientY - muteButton.offsetTop;
+//   // muteButton.style.pointerEvents = 'none';
+// });
 
-function toggleMute() {
-  if (video.muted) {
-    video.muted = false;
-  } else {
-    video.muted = true;
-  }
-}
+// document.addEventListener("mousemove", function (e) {
+//   if (moving) {
+//     muteButton.style.left = e.clientX - offsetX + "px";
+//     muteButton.style.top = e.clientY - offsetY + "px";
+//   }
+// });
+
+// document.addEventListener("mouseup", function () {
+//   moving = false;
+//   // muteButton.style.pointerEvents = 'none';
+// });
+
+// function toggleMute() {
+//   if (video.muted) {
+//     video.muted = false;
+//   } else {
+//     video.muted = true;
+//   }
+// }
